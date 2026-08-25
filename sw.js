9df13356aa7887640aad7c6bc18beec3eba6a069
punishconcept-create <punishconcept@gmail.com>
2026-08-25 18:23:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,22 @@
-const CACHE_NAME='gentleman-master-scanner-v1';
-const SHELL=['./','./index.html','./scanner.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE_NAME="gentleman-pwa-local-v7";
+const SHELL=[
+  "./",
+  "./index.html",
+  "./create.html",
+  "./manage.html",
+  "./scanner.html",
+  "./admin.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-self.addEventListener('install',event=>{
+self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(SHELL)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate',event=>{
+self.addEventListener("activate",event=>{
   event.waitUntil(
     caches.keys().then(keys=>Promise.all(
       keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
@@ -15,22 +25,24 @@ self.addEventListener('activate',event=>{
   self.clients.claim();
 });
 
-self.addEventListener('fetch',event=>{
+self.addEventListener("fetch",event=>{
   const url=new URL(event.request.url);
   if(url.origin!==self.location.origin) return;
 
-  if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));
+  if(event.request.mode==="navigate"){
+    event.respondWith(
+      fetch(event.request).catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html")))
+    );
     return;
   }
 
   event.respondWith(
-    caches.match(event.request).then(cached=>{
-      return cached || fetch(event.request).then(response=>{
+    caches.match(event.request).then(cached=>
+      cached || fetch(event.request).then(response=>{
         const copy=response.clone();
         caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
         return response;
-      });
-    })
+      })
+    )
   );
 });
